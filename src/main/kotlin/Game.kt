993bd7fd159501539f1ef21a106c7305d1820151
Alexander Gavrilyuk <one1970zero@mail.ru>
2023-06-26 17:55:47 +0300

    When

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -9,20 +9,38 @@ fun main(args: Array<String>) {
     val auraColor = if (auraVisible) "GREEN" else "NONE"
     println(auraColor)
 
-    val healthStatus = if (healthPoints == 100) {
-        "is in excellent condition!"
-    } else if (healthPoints in 90..99) {
-        "has a few scratches."
-    } else if (healthPoints in 75..89) {
-        if (isBlessed) {
+
+// if/else
+// --------------------------------------------------------------------------------------------------------------------------
+//    val healthStatus = if (healthPoints == 100) {
+//        "is in excellent condition!"
+//    } else if (healthPoints in 90..99) {
+//        "has a few scratches."
+//    } else if (healthPoints in 75..89) {
+//        if (isBlessed) {
+//            "has some minor wounds but is healing quite quickly!"
+//        } else {
+//            "has some minor wounds."
+//            }
+//    } else if (healthPoints in 15..74) {
+//        "looks pretty hurt."
+//    } else {
+//        "is in awful condition!" }
+//    // Состояние игрока
+//    println("$name $healthStatus")
+
+//    when
+//----------------------------------------------------------------------------------------------------------------------------
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
             "has some minor wounds but is healing quite quickly!"
         } else {
             "has some minor wounds."
-            }
-    } else if (healthPoints in 15..74) {
-        "looks pretty hurt."
-    } else {
-        "is in awful condition!" }
-    // Состояние игрока
-    println("$name $healthStatus")
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
+        println("$name $healthStatus")
 }
